0cd4bf40ad39d2dbdd1b5fa747f976910961c161
Tim Goll <tim@timgoll.de>
2020-02-10 17:53:34 +0100

    finished TID

--- lua/autorun/client/cl_weaponstats_tid.lua
+++ lua/autorun/client/cl_weaponstats_tid.lua
@@ -1,4 +1,10 @@
-local mat_tid_ammo = Material("vgui/ttt/tid_ammo")
+local mat_tid_acc = Material("vgui/ttt/tid/tid_accuracy")
+local mat_tid_ammo = Material("vgui/ttt/tid/tid_ammo")
+local mat_tid_auto = Material("vgui/ttt/tid/tid_automatic")
+local mat_tid_rec = Material("vgui/ttt/tid/tid_recoil")
+local mat_tid_speed = Material("vgui/ttt/tid/tid_spm")
+
+local mat_tid_large_ammo = Material("vgui/ttt/tid/tid_lg_ammo")
 
 local function GetAccuracyLangString(acc)
 	if acc <= 0.01 then
@@ -40,6 +46,11 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDWeaponStats", function(tData)
 	if ent:Clip1() ~= -1 then
 		ammomax = (ent.Primary.ClipMax == -1) and "ttt2_wstat_no_ammo" or ent.Primary.ClipMax
 
+		-- add an empty line if there's already data in the description area
+		if tData:GetAmountDescriptionLines() > 0 then
+			tData:AddDescriptionLine()
+		end
+
 		tData:AddDescriptionLine(
 			LANG.GetParamTranslation("ttt2_wstat_ammo", {
 				ammo = ent:GetNWInt("ttt2_wepstat_stored_ammo", 0),
@@ -53,39 +64,66 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDWeaponStats", function(tData)
 		tData:AddDescriptionLine(
 			LANG.GetParamTranslation("ttt2_wstat_acc", {acc = LANG.TryTranslation(GetAccuracyLangString(ent.Primary.Cone))}),
 			nil,
-			{mat_tid_ammo}
+			{mat_tid_acc}
 		)
 
 		tData:AddDescriptionLine(
 			LANG.GetParamTranslation("ttt2_wstat_rec", {rec = LANG.TryTranslation(GetRecoilLangString(ent.Primary.Recoil))}),
 			nil,
-			{mat_tid_ammo}
+			{mat_tid_rec}
 		)
 
 		if ent.Primary.Automatic then
 			tData:AddDescriptionLine(
 				LANG.TryTranslation("ttt2_wstat_auto"),
 				nil,
-				{mat_tid_ammo}
+				{mat_tid_auto}
 			)
 		else
 			tData:AddDescriptionLine(
 				LANG.TryTranslation("ttt2_wstat_not_auto"),
 				nil,
-				{mat_tid_ammo}
+				{mat_tid_auto}
 			)
 		end
 
 		tData:AddDescriptionLine(
 			LANG.GetParamTranslation("ttt2_wstat_speed", {rate = math.Round(60 / ent.Primary.Delay)}),
 			nil,
-			{mat_tid_ammo}
+			{mat_tid_speed}
 		)
 	end
 end)
 
+local ammo_types = {
+	["item_ammo_357_ttt"] = true,
+	["item_box_buckshot_ttt"] = true,
+	["item_ammo_smg1_ttt"] = true,
+	["item_ammo_pistol_ttt"] = true,
+	["item_ammo_revolver_ttt"] = true
+}
+
+hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDAmmoBoxes", function(tData)
+	local client = LocalPlayer()
+	local ent = tData:GetEntity()
+
+	if not IsValid(client) or not client:IsTerror() or not client:Alive()
+	or not IsValid(ent) or tData:GetEntityDistance() > 100 or not ammo_types[ent:GetClass()] then
+		return
+	end
+
+	-- enable targetID rendering
+	tData:EnableText()
+	tData:EnableOutline()
+	tData:SetOutlineColor(client:GetRoleColor())
+
+	tData:SetTitle(LANG.TryTranslation("ttt2_wstat_ammo_name"))
+	tData:SetSubtitle(LANG.TryTranslation("ttt2_wstat_ammo_walk_over"))
+	tData:AddIcon(mat_tid_large_ammo)
+end)
+
 hook.Add("Initialize", "ttt2_init_weaponstat_lang", function()
-	LANG.AddToLanguage("English", "ttt2_wstat_ammo", "Stored Ammunition: {clip} + {ammo} (max: {ammomax})")
+	LANG.AddToLanguage("English", "ttt2_wstat_ammo", "Stored ammunition: {clip} + {ammo} (max: {ammomax})")
 	LANG.AddToLanguage("Deutsch", "ttt2_wstat_ammo", "Enthaltene Munition: {clip} + {ammo} (max: {ammomax})")
 
 	LANG.AddToLanguage("English", "ttt2_wstat_acc", "Accuracy: {acc}")
@@ -94,8 +132,8 @@ hook.Add("Initialize", "ttt2_init_weaponstat_lang", function()
 	LANG.AddToLanguage("English", "ttt2_wstat_rec", "Recoil: {rec}")
 	LANG.AddToLanguage("Deutsch", "ttt2_wstat_rec", "Rückstoß: {rec}")
 
-	LANG.AddToLanguage("English", "ttt2_wstat_auto", "Automatic Weapon")
-	LANG.AddToLanguage("Deutsch", "ttt2_wstat_auto", "AUtomatische Waffe")
+	LANG.AddToLanguage("English", "ttt2_wstat_auto", "Automatic weapon")
+	LANG.AddToLanguage("Deutsch", "ttt2_wstat_auto", "Automatische Waffe")
 
 	LANG.AddToLanguage("English", "ttt2_wstat_not_auto", "Non automatic weapon")
 	LANG.AddToLanguage("Deutsch", "ttt2_wstat_not_auto", "Nichtautomatische Waffe")
@@ -141,4 +179,12 @@ hook.Add("Initialize", "ttt2_init_weaponstat_lang", function()
 
 	LANG.AddToLanguage("English", "ttt2_wstat_rec_veryhigh", "very high")
 	LANG.AddToLanguage("Deutsch", "ttt2_wstat_rec_veryhigh", "Sehr hoch")
+
+
+
+	LANG.AddToLanguage("English", "ttt2_wstat_ammo_name", "Ammo Box")
+	LANG.AddToLanguage("Deutsch", "ttt2_wstat_ammo_name", "Munitionskiste")
+
+	LANG.AddToLanguage("English", "ttt2_wstat_ammo_walk_over", "Walk over to pick up.")
+	LANG.AddToLanguage("Deutsch", "ttt2_wstat_ammo_walk_over", "Drüberlaufen um aufzusammeln.")
 end)
--- lua/autorun/server/sv_weaponstats_setup.lua
+++ lua/autorun/server/sv_weaponstats_setup.lua
@@ -1,1 +1,7 @@
-resource.AddFile("materials/vgui/ttt/tid_ammo.vmt")
+resource.AddFile("materials/vgui/ttt/tid/tid_accuracy.vmt")
+resource.AddFile("materials/vgui/ttt/tid/tid_ammo.vmt")
+resource.AddFile("materials/vgui/ttt/tid/tid_automatic.vmt")
+resource.AddFile("materials/vgui/ttt/tid/tid_recoil.vmt")
+resource.AddFile("materials/vgui/ttt/tid/tid_spm.vmt")
+
+resource.AddFile("materials/vgui/ttt/tid/tid_lg_ammo.vmt")
