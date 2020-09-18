4fa322436d5e19111a83e041c2a12dc41d114543
Tekiad <tekiadp@gmail.cm>
2020-09-18 13:19:15 -0300

    Create weaponstats.lua (#3)
    * Create weaponstats.lua
    
    * "spanish" to "Español"

---
+++ lua/lang/spanish/weaponstats.lua
@@ -0,0 +1,26 @@
+L = LANG.GetLanguageTableReference("Español")
+
+L["ttt2_wstat_ammo"] = "Munición guardada: {clip} + {ammo} (máx: {ammomax})"
+L["ttt2_wstat_dmg"] = "Daño: {dmg}"
+L["ttt2_wstat_acc"] = "Precisión: {acc}"
+L["ttt2_wstat_rec"] = "Retroceso: {rec}"
+L["ttt2_wstat_auto"] = "Arma automática"
+L["ttt2_wstat_not_auto"] = "Arma semi/manual"
+L["ttt2_wstat_speed"] = "Cadencia: {rate} TPM"
+
+L["ttt2_wstat_no_ammo"] = "no has recogido munición"
+
+L["ttt2_wstat_acc_verygood"] = "muy buena"
+L["ttt2_wstat_acc_good"] = "buena"
+L["ttt2_wstat_acc_average"] = "promedio"
+L["ttt2_wstat_acc_bad"] = "mala"
+L["ttt2_wstat_acc_verybad"] = "muy mala"
+
+L["ttt2_wstat_rec_verylow"] = "muy bajo"
+L["ttt2_wstat_rec_low"] = "bajo"
+L["ttt2_wstat_rec_average"] = "promedio"
+L["ttt2_wstat_rec_high"] = "alto"
+L["ttt2_wstat_rec_veryhigh"] = "muy alto"
+
+L["ttt2_wstat_ammo_name"] = "Caja de Munición"
+L["ttt2_wstat_ammo_walk_over"] = "Camina sobre ella para recogerla."
