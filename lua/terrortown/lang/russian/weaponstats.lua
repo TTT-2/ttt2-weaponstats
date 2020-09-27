841dd02a0e0c7c6c1ba0900754ef7525b6b28242
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 13:17:46 +0500

    Create weaponstats.lua

---
+++ lua/terrortown/lang/russian/weaponstats.lua
@@ -0,0 +1,26 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt2_wstat_ammo"] = "Боеприпасов: {clip} + {ammo} (макс.: {ammomax})"
+L["ttt2_wstat_dmg"] = "Урон: {dmg}"
+L["ttt2_wstat_acc"] = "Точность: {acc}"
+L["ttt2_wstat_rec"] = "Отдача: {rec}"
+L["ttt2_wstat_auto"] = "Автоматическое оружие"
+L["ttt2_wstat_not_auto"] = "Неавтоматическое оружие"
+L["ttt2_wstat_speed"] = "Скорострельность: {rate} ПВС"
+
+L["ttt2_wstat_no_ammo"] = "нет боеприпасов"
+
+L["ttt2_wstat_acc_verygood"] = "отличная"
+L["ttt2_wstat_acc_good"] = "хорошая"
+L["ttt2_wstat_acc_average"] = "средняя"
+L["ttt2_wstat_acc_bad"] = "плохая"
+L["ttt2_wstat_acc_verybad"] = "очень плохая"
+
+L["ttt2_wstat_rec_verylow"] = "очень низкая"
+L["ttt2_wstat_rec_low"] = "низкая"
+L["ttt2_wstat_rec_average"] = "средняя"
+L["ttt2_wstat_rec_high"] = "высокая"
+L["ttt2_wstat_rec_veryhigh"] = "очень высокая"
+
+L["ttt2_wstat_ammo_name"] = "Ящик с боеприпасами"
+L["ttt2_wstat_ammo_walk_over"] = "Подойдите, чтобы забрать."
