63007eed7d79d17831efd314d49ed524e3a2ba86
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-09 18:01:23 +0200

    Create Italian

---
+++ lua/terrortown/lang/italian/weaponstats.lua
@@ -0,0 +1,26 @@
+local L = LANG.GetLanguageTableReference("italiano")
+
+L["ttt2_wstat_ammo"] = "Munizioni immagazzinate: {clip} + {ammo} (max: {ammomax})"
+L["ttt2_wstat_dmg"] = "Danno: {dmg}"
+L["ttt2_wstat_acc"] = "Precisione: {acc}"
+L["ttt2_wstat_rec"] = "Rinculo: {rec}"
+L["ttt2_wstat_auto"] = "Arma Automatica"
+L["ttt2_wstat_not_auto"] = "Arma Non automatica"
+L["ttt2_wstat_speed"] = "Rateo di fuoco: {rate} SPM"
+
+L["ttt2_wstat_no_ammo"] = "No Munizioni"
+
+L["ttt2_wstat_acc_verygood"] = "molto buona"
+L["ttt2_wstat_acc_good"] = "buona"
+L["ttt2_wstat_acc_average"] = "media"
+L["ttt2_wstat_acc_bad"] = "scarsa"
+L["ttt2_wstat_acc_verybad"] = "molto scarsa"
+
+L["ttt2_wstat_rec_verylow"] = "molto basso"
+L["ttt2_wstat_rec_low"] = "basso"
+L["ttt2_wstat_rec_average"] = "medio"
+L["ttt2_wstat_rec_high"] = "alto"
+L["ttt2_wstat_rec_veryhigh"] = "molto alto"
+
+L["ttt2_wstat_ammo_name"] = "Pacco di munizioni"
+L["ttt2_wstat_ammo_walk_over"] = "Camminaci sopra per prenderlo."
