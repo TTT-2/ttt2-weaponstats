b9bff19b4d1c95560549f546885fea6ca28fc8a2
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-07-24 12:48:55 +0800

    Add files via upload

---
+++ lua/terrortown/lang/zh_hans/weaponstats.lua
@@ -0,0 +1,26 @@
+local L = LANG.GetLanguageTableReference("zh_hans")
+
+L["ttt2_wstat_ammo"] = "储存弹药: {clip} + {ammo} (最大值: {ammomax})"
+L["ttt2_wstat_dmg"] = "伤害: {dmg}"
+L["ttt2_wstat_acc"] = "准确度: {acc}"
+L["ttt2_wstat_rec"] = "后坐力: {rec}"
+L["ttt2_wstat_auto"] = "自动武器"
+L["ttt2_wstat_not_auto"] = "非自动武器"
+L["ttt2_wstat_speed"] = "射速: {rate} SPM"
+
+L["ttt2_wstat_no_ammo"] = "没有弹药拾取"
+
+L["ttt2_wstat_acc_verygood"] = "非常好"
+L["ttt2_wstat_acc_good"] = "好"
+L["ttt2_wstat_acc_average"] = "平均"
+L["ttt2_wstat_acc_bad"] = "坏"
+L["ttt2_wstat_acc_verybad"] = "非常糟糕"
+
+L["ttt2_wstat_rec_verylow"] = "非常低"
+L["ttt2_wstat_rec_low"] = "低"
+L["ttt2_wstat_rec_average"] = "平均"
+L["ttt2_wstat_rec_high"] = "高"
+L["ttt2_wstat_rec_veryhigh"] = "非常高"
+
+L["ttt2_wstat_ammo_name"] = "弹药箱"
+L["ttt2_wstat_ammo_walk_over"] = "走过去捡起."
