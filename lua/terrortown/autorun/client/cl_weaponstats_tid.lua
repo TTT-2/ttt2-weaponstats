89bc191623a3cc802df2013585a489fb98160d45
EntranceJew <EntranceJew@gmail.com>
2023-11-18 22:05:31 -0600

    use ammo icons and names

--- lua/terrortown/autorun/client/cl_weaponstats_tid.lua
+++ lua/terrortown/autorun/client/cl_weaponstats_tid.lua
@@ -7,18 +7,26 @@ local mat_tid_speed = Material("vgui/ttt/tid/tid_spm")
 
 local mat_tid_large_ammo = Material("vgui/ttt/tid/tid_lg_ammo")
 
+local ammo_type_icons = {
+	["357"] = Material("vgui/ttt/tid/tid_big_ammo_rifle"),
+	["Buckshot"] = Material("vgui/ttt/tid/tid_big_ammo_shotgun"),
+	["SMG1"] = Material("vgui/ttt/tid/tid_big_ammo_mac10"),
+	["Pistol"] = Material("vgui/ttt/tid/tid_big_ammo_pistol"),
+	["AlyxGun"] = Material("vgui/ttt/tid/tid_big_ammo_deagle"),
+}
+
 local function GetAccuracyLangString(weapon)
 	local acc = weapon.Primary.Cone
-	
+
 	-- special ArcCW weapon handling
 	if not acc and weapon.ArcCW and ArcCW then
 		acc = weapon:GetBuff("SightsDispersion")
 
 		if acc ~= 0 then -- not a sniper
-			acc = weapon:GetBuff("HipDispersion") * ArcCW.MOAToAcc / 10	
+			acc = weapon:GetBuff("HipDispersion") * ArcCW.MOAToAcc / 10
 		end
 	end
-	
+
 	if acc then
 		if acc <= 0.01 then
 			return "ttt2_wstat_acc_verygood"
@@ -32,19 +40,17 @@ local function GetAccuracyLangString(weapon)
 			return "ttt2_wstat_acc_verybad"
 		end
 	end
-	
+
 	return "ttt2_wstat_acc_average"
 end
 
 local function GetRecoilLangString(weapon)
 	local acc = weapon.Primary.Recoil
-	
-	if not acc then
-		if weapon.ArcCW and ArcCW then
-			acc = weapon.Recoil * 6
-		end
+
+	if not acc and weapon.ArcCW and ArcCW then
+		acc = weapon.Recoil * 6
 	end
-	
+
 	if acc then
 		if acc <= 2 then
 			return "ttt2_wstat_rec_verylow"
@@ -58,29 +64,29 @@ local function GetRecoilLangString(weapon)
 			return "ttt2_wstat_rec_veryhigh"
 		end
 	end
-	
+
 	return "ttt2_wstat_rec_average"
 end
 
 local function GetDamage(weapon)
 	local damage = weapon.Primary.Damage
-	
+
 	if damage ~= nil then return damage end
-	
+
 	damage = weapon.Damage
-	
+
 	if damage ~= nil then return damage end
-	
+
 	return "N/A"
 end
 
 local function GetDelay(weapon)
 	local delay = weapon.Primary.Delay
-	
+
 	if delay ~= nil then return delay end
-	
+
 	delay = weapon.Delay
-	
+
 	return delay
 end
 
@@ -112,6 +118,8 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDWeaponStats", function(tData)
 		tData:AddDescriptionLine()
 	end
 
+	local ammo_mat = ammo_type_icons[ent.Primary.Ammo] or mat_tid_ammo
+
 	tData:AddDescriptionLine(
 		LANG.GetParamTranslation("ttt2_wstat_ammo", {
 			ammo = ent:GetNWInt("ttt2_wepstat_stored_ammo", 0),
@@ -119,7 +127,7 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDWeaponStats", function(tData)
 			clip = clip1 or 0
 		}),
 		nil,
-		{mat_tid_ammo}
+		{ammo_mat}
 	)
 
 	tData:AddDescriptionLine(
@@ -153,9 +161,9 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDWeaponStats", function(tData)
 			{mat_tid_auto}
 		)
 	end
-		
+
 	local delay = GetDelay(ent)
-		
+
 	if delay then
 		tData:AddDescriptionLine(
 			LANG.GetParamTranslation("ttt2_wstat_speed", {rate = math.Round(60 / delay)}),
@@ -187,7 +195,7 @@ hook.Add("TTTRenderEntityInfo", "HUDDrawTargetIDAmmoBoxes", function(tData)
 	tData:EnableOutline()
 	tData:SetOutlineColor(client:GetRoleColor())
 
-	tData:SetTitle(LANG.TryTranslation("ttt2_wstat_ammo_name"))
+	tData:SetTitle(LANG.TryTranslation("ammo_" .. string.lower(ent.AmmoType)))
 	tData:SetSubtitle(LANG.TryTranslation("ttt2_wstat_ammo_walk_over"))
-	tData:AddIcon(mat_tid_large_ammo)
+	tData:AddIcon(ammo_type_icons[ent.AmmoType] or mat_tid_large_ammo)
 end)
