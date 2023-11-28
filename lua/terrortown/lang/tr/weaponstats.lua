369fb06ca8d41025a0d9caae6443107815bb1ed9
Talha Berkay Akbulut <49911006+NovaDiablox@users.noreply.github.com>
2023-11-28 15:39:07 +0300

    Added Turkish Language

---
+++ lua/terrortown/lang/tr/weaponstats.lua
@@ -0,0 +1,26 @@
+local L = LANG.GetLanguageTableReference("tr")
+
+L["ttt2_wstat_ammo"] = "Depolanan Cephane: {clip} + {ammo} (maks: {ammomax})"
+L["ttt2_wstat_dmg"] = "Hasar: {dmg}"
+L["ttt2_wstat_acc"] = "Tutarlılık: {acc}"
+L["ttt2_wstat_rec"] = "Geri Tepme: {rec}"
+L["ttt2_wstat_auto"] = "Otomatik silah"
+L["ttt2_wstat_not_auto"] = "Otomatik olmayan silah"
+L["ttt2_wstat_speed"] = "Ateş hızı: {rate} SPM"
+
+L["ttt2_wstat_no_ammo"] = "cephane alımı yok"
+
+L["ttt2_wstat_acc_verygood"] = "çok iyi"
+L["ttt2_wstat_acc_good"] = "iyi"
+L["ttt2_wstat_acc_average"] = "ortalama"
+L["ttt2_wstat_acc_bad"] = "kötü"
+L["ttt2_wstat_acc_verybad"] = "çok kötü"
+
+L["ttt2_wstat_rec_verylow"] = "çok düşük"
+L["ttt2_wstat_rec_low"] = "düşük"
+L["ttt2_wstat_rec_average"] = "ortalama"
+L["ttt2_wstat_rec_high"] = "yüksek"
+L["ttt2_wstat_rec_veryhigh"] = "çok yüksek"
+
+L["ttt2_wstat_ammo_name"] = "Cephane Kutusu"
+L["ttt2_wstat_ammo_walk_over"] = "Almak için üstüne yürüyün."
